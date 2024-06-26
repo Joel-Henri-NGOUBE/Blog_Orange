331ca09a4f1e12f3bc0e517eaa954a05f79a4156
Joël Henri <ebosqua.edimo@gmail.com>
2024-06-27 00:24:12 +0200

    Templates, vues et modèles

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. 
+class User(models.Model):
+    username: models.CharField = models.CharField(max_length=200)
+    mail: models.EmailField = models.EmailField(null=True)
+    password: models.CharField = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.username
+    
+class Article(models.Model):
+    title: models.CharField = models.CharField(max_length=200, null=False)
+    content: models.TextField = models.TextField(null=False)
+    publication_date: models.TimeField = models.DateTimeField(auto_now_add=True, null=False)
+    user: models.ForeignKey = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    
+    def __str__(self):
+        return self.title
+
--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+def articles(request):
+    articles = Article.objects.all()
+    context: dict = {
+        "articles": articles
+    }
+    return render(request, "articles/index.html", context = context)

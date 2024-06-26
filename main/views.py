5e90a067703b88ffc333331880ca9194f3e8f147
Joël Henri <ebosqua.edimo@gmail.com>
2024-06-27 01:26:18 +0200

    Internationalisation entamée

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Article
+from django.utils.translation import gettext
 
 # Create your views here.
 def articles(request):
+    bonjour = gettext("bonjour")
     articles = Article.objects.all()
     context: dict = {
         "articles": articles
